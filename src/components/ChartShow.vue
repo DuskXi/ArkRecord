f6db7cb8b4718a83e5bb6fed9a578dc1e3605235
DuskXi <84715902+DuskXi@users.noreply.github.com>
2022-07-16 17:21:09 +0100

    微调

--- src/components/ChartShow.vue
+++ src/components/ChartShow.vue
@@ -6,13 +6,13 @@
   </div>
   <lineChart :pools="pools" :detail="enableTimeChartDetails" :count="enableTimeChartCount"></lineChart>
 
-  <div  ref="printMe">
-  <div class="text-h5 vertical-middle	">卡池出货比例</div>
-  <div class="q-gutter-sm">
-    <q-toggle v-model="enablePoolsBarChartDetails" label="显示详情"/>
-    <q-toggle v-if="enablePoolsBarChartDetails" v-model="enablePoolsBarChartCount" label="详细统计"/>
-  </div>
-  <barChart :pools="pools" :detail="enablePoolsBarChartDetails" :count="enablePoolsBarChartCount"></barChart>
+  <div ref="printMe">
+    <div class="text-h5 vertical-middle	">卡池出货比例</div>
+    <div class="q-gutter-sm">
+      <q-toggle v-model="enablePoolsBarChartDetails" label="显示详情"/>
+      <q-toggle v-if="enablePoolsBarChartDetails" v-model="enablePoolsBarChartCount" label="详细统计"/>
+    </div>
+    <barChart :pools="pools" :detail="enablePoolsBarChartDetails" :count="enablePoolsBarChartCount"></barChart>
   </div>
   <div class="text-h5 vertical-middle	">自定义限制饼图</div>
   <pieChart :pools="pools"></pieChart>
@@ -37,17 +37,22 @@ export default {
   },
   methods: {
     async loadData() {
-      let rawData = await readLocalStorage("ArknightsCardInformation");
+      let rawData = await readLocalStorage(this.bilibili ? "ArknightsCardInformationB" : "ArknightsCardInformation");
       this.poolsDict = loadPools(rawData);
       this.pools = Object.values(this.poolsDict);
       this.pools.sort((a, b) => {
         return b.getLastUpdate() - a.getLastUpdate();
       });
     },
-
   },
   async mounted() {
     await this.loadData();
+  },
+  props: {
+    bilibili: {
+      type: Boolean,
+      default: false
+    }
   },
   data: () => ({
     output: null,
